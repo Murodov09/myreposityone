75e1eb70d265ffd7899f055cba792e8f182c4cab
Murodov Mehriddin <145197930+Murodov09@users.noreply.github.com>
2023-11-14 14:59:31 +0300

    onecommit

---
+++ style.js
@@ -0,0 +1,21 @@
+//let a = +prompt("1-sonni yozing");
+//let b = +prompt("2-sonni yozing");
+//console.log(a+b);
+
+
+
+//let c = prompt("Email kiriting");
+//
+//if(c.length >=4 && c.includes("@gmail.com")){
+//    console.log("Emailingiz to'g'ri")
+//}else{
+//    console.log("Emailingiz xato");
+//}
+ 
+
+
+
+
+
+
+
